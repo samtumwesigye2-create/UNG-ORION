8cb7cd9e45c801ccded6b9da3aa607e5cacc8824
samtumwesigye2-create <samtumwesigye2@gmail.com>
2026-09-23 03:14:51 -0400

    Fix deployed frame engine import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from ung_shared.system_adapter import register_frame, convert_position, link_timing
+from app.frame_propagation import register_frame, convert_position, link_timing
 import os
 import json
 from datetime import datetime, timezone
